d9a32fbca32699c4be49f7c514c02b076c58f4e6
karolencina <karolencina@icloud.com>
2022-08-30 23:42:28 +0100

    Display array items as menu items in HTML

--- menu/app.js
+++ menu/app.js
@@ -5,7 +5,7 @@ const menu = [
         title: "chilli sin carne",
         category: "lunch",
         price: 15.99,
-        img: "./images/item-1.jpeg",
+        img: "./images/item-1.jpg",
         desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
@@ -13,23 +13,23 @@ const menu = [
         title: "banana chia",
         category: "breakfast",
         price: 13.99,
-        img: "./images/item-2.jpeg",
-        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+        img: "./images/item-2.jpg",
+        desc: `Vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
         id: 3,
         title: "avocado toast",
         category: "brunch",
         price: 6.99,
-        img: "./images/item-3.jpeg",
-        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+        img: "./images/item-3.jpg",
+        desc: `Kombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
     {
         id: 4,
         title: "smoothie bowl",
         category: "breakfast",
         price: 20.99,
-        img: "./images/item-4.jpeg",
+        img: "./images/item-4.jpg",
         desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
     },
     {
@@ -37,15 +37,15 @@ const menu = [
         title: "veggie noodles",
         category: "lunch",
         price: 22.99,
-        img: "./images/item-5.jpeg",
-        desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+        img: "./images/item-5.jpg",
+        desc: `Franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
     },
     {
         id: 6,
         title: "miso soup",
         category: "soups",
         price: 8.99,
-        img: "./images/item-6.jpeg",
+        img: "./images/item-6.jpg",
         desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
     },
     {
@@ -53,31 +53,50 @@ const menu = [
         title: "carrot spinach noodles",
         category: "lunch",
         price: 8.99,
-        img: "./images/item-7.jpeg",
-        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+        img: "./images/item-7.jpg",
+        desc: `Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
     },
     {
         id: 8,
         title: "broccoli shroom spaghetti",
         category: "lunch",
         price: 12.99,
-        img: "./images/item-8.jpeg",
-        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+        img: "./images/item-8.jpg",
+        desc: `On it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
     },
     {
         id: 9,
         title: "burrata bake",
         category: "appetisers",
         price: 16.99,
-        img: "./images/item-9.jpeg",
-        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+        img: "./images/item-9.jpg",
+        desc: `Skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
     {
         id: 10,
         title: "korean pancakes",
         category: "desserts",
         price: 16.99,
-        img: "./images/item-10.jpeg",
+        img: "./images/item-10.jpg",
         desc: `Amongst moments do in arrived at my replied. Fat weddings servants but man believed prospect. Companions understood is as especially pianoforte connection introduced.`,
     },
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function () {
+    let displayMenu = menu.map(function(item) {
+        return `<article class="menu-item">
+                <img src=${item.img} alt=${item.title} class="photo">
+                <div class="item-info">
+                  <header>
+                    <h4>${item.title}</h4>
+                    <h4 class="price">${item.price}</h4>
+                  </header>
+                  <p class="item-text">${item.desc}</p>
+                </div>
+              </article>`;
+    });
+    displayMenu = displayMenu.join('');
+    sectionCenter.innerHTML = displayMenu;
+});
