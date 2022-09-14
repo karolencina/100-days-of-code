70fb4173e818ff745161129f603215ec050f9335
karolencina <karolencina@icloud.com>
2022-09-14 12:10:50 +0100

    [Scroll] Dynamic height navbar toggle

--- scroll/app.js
+++ scroll/app.js
@@ -1,3 +1,18 @@
 /* Set date */
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+/* Close links */
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
