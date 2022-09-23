641e471861aed215b43cc44f73df77e9b02fd566
karolencina <karolencina@icloud.com>
2022-09-23 11:42:41 +0100

    [Countdown] Displayed subtitle date dynamically

--- countdown/app.js
+++ countdown/app.js
@@ -12,7 +12,6 @@ const months = [
   "November",
   "December",
 ];
-
 const weekdays = [
   "Sunday",
   "Monday",
@@ -26,3 +25,21 @@ const weekdays = [
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const timeUnits = document.querySelectorAll(".time-unit");
+
+let futureDate = new Date(2023, 3, 25, 11, 30, 00);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+console.log(month);
+
+/* Because futureDate.getDay() returns an integer, it can be used to access a value in an array - so intead of using weekdays[3], we can use weekdays[futureDate.getDay()] */
+
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am.`;
