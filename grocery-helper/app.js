402d60441880064152838aa59871f20b3d4ae70f
karolencina <karolencina@icloud.com>
2022-09-26 21:26:02 +0100

    [Grocery Helper] Set up consts and lets

---
+++ grocery-helper/app.js
@@ -0,0 +1,26 @@
+/* Select items */
+
+const alert = document.querySelector(".alert");
+// Basically a container for alerts, title, input field and submit button
+const form = document.querySelector(".grocery-form");
+// Input field
+const groceryInput = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+// Container for all items that were already added
+const groceriesContainer = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+/* Edit option */
+
+let editItem;
+let editFlag = false;
+let editId = "";
+
+/* Event listeners */
+
+/* Functions */
+
+/* Local storage */
+
+/* Setup items */
