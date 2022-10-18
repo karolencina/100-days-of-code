430301cba8d7c355651e1c7a83a599b65817937c
karolencina <karolencina@icloud.com>
2022-10-18 21:38:06 +0100

    [Servings Changer] Rewrite jQuery to JS

--- servings-changer/app.js
+++ servings-changer/app.js
@@ -1,17 +1,20 @@
-$(function () {
-  $(".serving").bind("change", function (event) {
-    let previousValue = parseFloat($("#defaultServing").val());
-    let newValue = parseFloat($(event.target).val());
-    if (previousValue && newValue) {
-      $(".ingredient").each(function (index, elem) {
-        let ingrCurrent = $(".amount", elem);
-        let oldIngredientAmount = ingrCurrent.text();
-        console.log(oldIngredientAmount);
-        let newIngredientAmount =
-          (oldIngredientAmount * newValue) / previousValue;
-        ingrCurrent.text(newIngredientAmount);
-      });
-      $("#defaultServing").val(newValue);
-    }
-  });
+const servingInput = document.querySelector("#serving");
+servingInput.addEventListener("change", (event) => {
+  let defaultServing = document.querySelector("#defaultServing").value;
+  let previousValue = parseInt(defaultServing);
+  let newValue = parseInt(event.target.value);
+  if (previousValue && newValue) {
+    const ingredients = document.querySelectorAll(".ingredient");
+    ingredients.forEach(function (element) {
+      let quantity = element.querySelector(".quantity");
+      console.log(quantity);
+      let oldQuantity = quantity.innerHTML;
+      let newQuantity = (oldQuantity * newValue) / previousValue;
+      console.log(oldQuantity);
+      console.log(newValue);
+      console.log(previousValue);
+      quantity.innerHTML = newQuantity;
+    });
+    document.querySelector("#defaultServing").value = newValue;
+  }
 });
